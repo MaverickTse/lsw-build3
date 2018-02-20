e84646d66e02a0bc2bfbed0b1ce25bb6deed9035
MaverickTse <mavericktse@gmail.com>
2018-02-20 16:27:56 +0800

    Initial upload

---
+++ buildme.sh
@@ -0,0 +1,507 @@
+#!/usr/bin/bash
+
+FORCE_REBUILD=0
+
+PATH_SANDBOX="$HOME/Sandbox"
+PATH_SRC=$PATH_SANDBOX"/Src"
+PATH_BLD=$PATH_SANDBOX"/BLD32"
+PATH_PREFIX=$PATH_SANDBOX"/Install"
+
+URL_ZLIB="https://github.com/madler/zlib.git"
+URL_LCMS="https://github.com/mm2/Little-CMS.git"
+URL_LIBPNG="https://github.com/glennrp/libpng.git"
+URL_LIBTIFF="https://gitlab.com/libtiff/libtiff.git"
+URL_OPENJPEG="https://github.com/uclouvain/openjpeg.git"
+URL_LSMASH="https://github.com/l-smash/l-smash.git"
+URL_FFMPEG="https://github.com/FFmpeg/FFmpeg.git"
+URL_LSW="https://github.com/VFR-maniac/L-SMASH-Works.git"
+
+printf "L-SMASH Works and FFmpeg Build Script (32bit) for MSYS2\n"
+export MSYSTEM="MINGW32"
+export MINGW_MOUNT_POINT="/i686-posix-sjlj"
+export PATH="${MINGW_MOUNT_POINT}/bin:${PATH_PREFIX}/bin:$PATH"
+export PKG_CONFIG_PATH="${PATH_PREFIX}/lib/pkgconfig:${PATH_PREFIX}/share/pkgconfig"
+export ACLOCAL_PATH="${MINGW_MOUNT_POINT}/share/aclocal:/usr/share/aclocal"
+
+export CC=gcc
+export CXX=g++
+export AR=ar
+export WINDRES=windres
+
+# NOTE: Uncommenting the following exports may cause build failures
+#export CFLAGS=" -static -static-libgcc"
+#export CPPFLAGS=" -static -static-libgcc -static-libstdc++"
+#export CXXFLAGS=" -static -static-libgcc -static-libstdc++"
+#export LDFLAGS=" -static "
+
+
+GetDir()
+# Get the path up to the directory
+# Args:
+#   $1: full/relative path
+# Returns:
+#	path to folder
+{
+	echo $(dirname $(readlink -f "$1"))
+}
+
+GetFilename()
+# Get the filename from path
+# Args:
+#   $1: full/relative path
+# Returns:
+#	filename
+{
+	echo $(basename $(readlink -f "$1"))
+}
+
+BuildSystem()
+# Guess what build system the folder is using
+# Give preference to Cmake over autotools
+# Args:
+#   $1: folder path
+# Returns:
+#    1: CMAKE
+#    2: Autotools with configure
+#    4: Autotools without configure but with aclocal
+#    8: A plain makefile
+#  225: Unknown
+{
+	if [[ ! -d "$1" ]]; then
+#		printf "%s is Missing" $1;
+		echo 0;
+		exit 0;
+	fi
+	
+	if [[ -e "$1/CMakeLists.txt" ]]; then
+#		printf "CMAKE";
+		echo 1
+		return;
+	fi
+	
+	if [[ -e "$1/configure" ]]; then
+#		printf "Configure";
+		echo 2;
+		return;
+	fi
+	
+	if [[ -e "$1/aclocal.m4" ]]; then
+#		printf "Autoreconf";
+		echo 4;
+		return;
+	fi
+	
+	if [[ -e "$1/Makefile" ]]; then
+#		printf "Make";
+		echo 8;
+		return;
+	fi
+#	printf "UNKNOWN";
+	echo 255;
+}
+
+IsSVN()
+# Determine if a URL points to a GIT/Subversion/Mercurial
+# Args:
+#   $1: the URL
+# Returns:
+#	0: No using version-management
+#	1: GIT
+#	2: Subversion
+#	4: Mercurial
+{
+	if [[ $1 = *git* ]]; then
+		echo 1;
+		return;
+	fi
+	if [[ $1 = *svn* ]]; then
+		echo 2;
+		return;
+	fi
+	if [[ $1 = *hg* ]]; then
+		echo 4;
+		return;
+	fi
+	if [[ $1 = *mercurial* ]]; then
+		echo 4;
+		return;
+	fi
+	echo 0;
+}
+
+CreateFolder()
+# Create the specified folder if missing
+# Args:
+#	$1: folder path
+# Returns:
+#	Exit if mkdir fails
+{
+	if [[ ! -d "$1" ]]; then
+		mkdir -p "$1" || exit 1;
+	fi
+}
+
+GitGet()
+# Clone or update Git repo
+# Args:
+#	$1: package name
+#	$2: URL
+# Returns:
+#	return 1 if newly cloned or updated
+{
+	local flag=0
+	local pkgname=$1
+	local url=$2
+	local srcdir="$PATH_SRC/$pkgname"
+	local originaldir=$(pwd)
+	if [[ ! -d "$srcdir" ]]; then
+		git clone --recursive $url $srcdir || exit 1;
+		flag=1;
+	else
+		cd "$srcdir";
+		local response=$(git pull|head -n1);
+		if [[ $response != *Already* ]]; then
+			flag=1;
+		fi
+	fi
+	cd "$originaldir";
+	echo $flag;
+}
+
+SVNGet()
+# Clone or update Subversion repo
+# Args:
+#	$1: package name
+#	$2: URL
+# Returns:
+#	return 1 if newly cloned or updated
+{
+	local flag=0
+	local pkgname=$1
+	local url=$2
+	local srcdir="$PATH_SRC/$pkgname"
+	local originaldir=$(pwd)
+	if [[ ! -d "$srcdir" ]]; then
+		svn checkout $url $srcdir || exit 1;
+		flag=1;
+	else
+		cd "$srcdir";
+		local response=$(svn update|wc -l);
+		if [[ $response -gt 2 ]]; then
+			flag=1;
+		fi
+	fi
+	cd "$originaldir";
+	echo $flag;
+}
+
+HGGet()
+# Clone or update Mercurial repo
+# Args:
+#	$1: package name
+#	$2: URL
+# Returns:
+#	return 1 if newly cloned or updated
+{
+	local flag=0
+	local pkgname=$1
+	local url=$2
+	local srcdir="$PATH_SRC/$pkgname"
+	local originaldir=$(pwd)
+	if [[ ! -d "$srcdir" ]]; then
+		hg clone $url $srcdir || exit 1;
+		flag=1;
+	else
+		cd "$srcdir";
+		local response=$(hg pull -u|wc -l);
+		if [[ $response -gt 3 ]]; then
+			flag=1;
+		fi
+	fi
+	cd "$originaldir";
+	echo $flag;
+}
+
+RepoGet()
+# A Wrapper for GitGet, SVNGet and HGGet
+# Calls approriate function depending on the URL
+# Args:
+#	$1: package name
+#	$2: URL
+# Returns:
+#	return 1 if newly cloned or updated
+{
+	local pkgname=$1
+	local url=$2
+	local repoflag=$(IsSVN $url)
+	local buildflag=0
+	case $repoflag in
+	"1")
+		buildflag=$(GitGet $pkgname $url)
+		;;
+	"2")
+		buildflag=$(SVNGet $pkgname $url)
+		;;
+	"3"|"4")
+		buildflag=$(HGGet $pkgname $url)
+		;;
+	*)
+		echo "Not a repo URL";
+		exit 1;
+		;;
+	esac
+	echo $buildflag
+}
+
+ArchiveGet()
+# Download source archives, i.e. tar, zip, 7z...
+# Depends on atool
+# Args:
+#	$1: package name
+#	$2: URL
+# Returns:
+#	Always returns 1
+{
+	local pkgname=$1
+	local url=$2
+	local dltemp="$PATH_SANDBOX\DL"
+	local originalfolder=$(pwd)
+	if [[ -d "$dltemp" ]]; then
+		rm -rf "$dltemp";
+	fi
+	CreateFolder "$dltemp"
+	cd "$dltemp"
+	wget "$url"
+	local archivename=$(ls -p | grep -v '/$' | head -n1)
+	atool -X ./expanded "$archivename"
+	local cmakecount=$(find -O2 . -iwholename cmakelists.txt| wc -l)
+	local configurecount=$(find -O2 . -iwholename configure| wc -l)
+	local makecount=$(find -O2 . -iname makefile| wc -l)
+	local firsttarget=""
+	if [[ $cmakecount -gt 0 ]]; then
+		firsttarget=$(find -O2 . -iwholename cmakelists.txt -printf "%d %p\n"|sort -n|perl -pe 's/^\d+\s//;'|head -n1)
+	elif [[ $configurecount -gt 0 ]]; then
+		firsttarget=$(find -O2 . -iwholename configure -printf "%d %p\n"|sort -n|perl -pe 's/^\d+\s//;'|head -n1)
+	elif [[ $makecount -gt 0 ]]; then
+		firsttarget=$(find -O2 . -iname makefile -printf "%d %p\n"|sort -n|perl -pe 's/^\d+\s//;'|head -n1)
+	else
+		exit 1
+	fi
+	local foldertomove=$(GetDir "$firsttarget")
+	local movedest="$PATH_SRC/$pkgname"
+	mv "$foldertomove" "$movedest" || exit 1
+	cd "$originalfolder"
+	rm -rf "$dltemp"
+	echo 1
+}
+
+BuildPackage()
+# Attempts to build a package
+# Args:
+#	$1: package name, no space
+#	$2: Repo or archive URL
+#	$3: extra arguments for CMake/configure
+#	$4: set to 1 to disable out-of-source build
+#	$5: set to 1 to disable Install step
+#	$6: folder containing patch(es)
+#	$7: set to 1 to force rebuild
+# Returns:
+#	None. This either fails and exits or proceed on success
+{
+	local pkgname=$1
+	local url=$2
+	local args=$3
+	local outsrc=$4
+	local noinst=$5
+	local patchfolder=$6
+	local rebuild=$7
+	local srcfolder="$PATH_SRC/$pkgname"
+	
+	# Download and extract stuff
+	local urltype=$(IsSVN $url)
+	local buildflag=0
+	cd "$HOME"
+	case $urltype in
+	"1"|"2"|"4")
+		buildflag=$(RepoGet $pkgname $url)
+		;;
+	*)
+		buildflag=$(ArchiveGet $pkgname $url)
+		;;
+	esac
+	
+	if [[ $rebuild -eq 1 ]]; then
+		buildflag=1
+	fi
+	
+	local buildsys=$(BuildSystem $srcfolder)
+	echo "buildflag=${buildflag}"
+	echo "buildsys=${buildsys}"
+	# Patching
+	if [[ -d "$patchfolder" ]]; then
+		cp $patchfolder\*.patch $srcfolder
+		cd "$srcfolder"
+		for pfile in *.patch
+		do
+			patch -p0 -b -t -i $pfile
+		done
+		rm *.patch
+		cd "$HOME"
+	fi
+	
+	# Building
+	if [[ $outsrc -eq 1 ]]; then
+		cd "$srcfolder"
+		case $buildsys in
+		"1")
+			if [[ $buildflag -eq 1 ]]; then
+			cmake \
+			 -G"MSYS Makefiles" \
+			 -DCMAKE_INSTALL_PREFIX="${PATH_PREFIX}" \
+			 -DCMAKE_BUILD_TYPE=Release \
+			 $args || exit $?;
+			 
+			 make -j$(nproc) || exit $?;
+			 fi
+			 if [[ $noinst -ne 1 ]]; then
+				make install || exit $?;
+			 fi			 
+			 ;;
+		"2")
+			if [[ $buildflag -eq 1 ]]; then
+			./configure --prefix=$PATH_PREFIX  $args || exit $?;
+			make -j$(nproc) || exit $?;
+			fi
+			if [[ $noinst -ne 1 ]]; then
+				make install || exit $?;
+			fi
+			;;
+		"4")
+			if [[ $buildflag -eq 1 ]]; then
+			autoreconf -ivf || exit $?
+			./configure --prefix=$PATH_PREFIX  $args || exit $?;
+			make -j$(nproc) || exit $?;
+			fi
+			if [[ $noinst -ne 1 ]]; then
+				make install || exit $?;
+			fi
+			;;
+		"8")
+			if [[ $buildflag -eq 1 ]]; then
+			$args make -j$(nproc) || exit $?;
+			fi
+			if [[ $noinst -ne 1 ]]; then
+				make install || exit $?;
+			fi
+			;;
+		*)
+			exit 1;
+			;;
+		esac
+	else
+		local bldfolder="$PATH_BLD/$pkgname";
+		CreateFolder "$bldfolder";
+		cd "$bldfolder";
+		case $buildsys in
+		"1")
+			if [[ $buildflag -eq 1 ]]; then
+			cmake \
+			 -G"MSYS Makefiles" \
+			 -DCMAKE_INSTALL_PREFIX="${PATH_PREFIX}" \
+			 -DCMAKE_BUILD_TYPE=Release \
+			 $args $srcfolder || exit $?;
+			 
+			make -j$(nproc) || exit $?;
+			fi
+			if [[ $noinst -ne 1 ]]; then
+				make install || exit $?;
+			fi
+			;;
+		"2")
+			if [[ $buildflag -eq 1 ]]; then
+			source ${srcfolder}/configure --prefix=$PATH_PREFIX  $args || exit $?;
+			make -j$(nproc) || exit $?;
+			fi
+			if [[ $noinst -ne 1 ]]; then
+				make install || exit $?;
+			fi
+			;;
+		"4")
+			if [[ $buildflag -eq 1 ]]; then
+			cd "$srcfolder";
+			autoreconf -ivf || exit $?;
+			cd "$bldfolder";
+			source ${srcfolder}/configure --prefix=$PATH_PREFIX  $args || exit $?;
+			make -j$(nproc) || exit $?;
+			fi
+			if [[ $noinst -ne 1 ]]; then
+				make install || exit $?;
+			fi
+			;;
+		*)
+			# Note: a MakeFile-only build system usually can only be run in source folder
+			# Correct me if I am wrong
+			exit 1;
+			;;
+		esac
+	fi
+	cd "$HOME"
+		
+}
+
+PkgZLIB()
+{
+	cd $HOME
+	BuildPackage zlib $URL_ZLIB "" 0 0 "" $FORCE_REBUILD
+	pkg-config zlib --libs 
+}
+
+PkgLSMASH()
+{
+	cd $HOME
+	BuildPackage lsmash $URL_LSMASH "" 1 0 "" $FORCE_REBUILD
+	pkg-config liblsmash --libs 
+}
+
+PkgFFMPEG()
+{
+	cd $HOME
+	echo Building FFmpeg libraries. This will take quite a while.
+	BuildPackage ffmpeg $URL_FFMPEG " --target-os=mingw32 --disable-shared --disable-debug --disable-programs --disable-doc --enable-static --enable-avresample --enable-gpl --enable-version3 --enable-runtime-cpudetect --enable-avisynth " 1 0 "" $FORCE_REBUILD
+		
+}
+
+PkgLSWAVIUTL()
+{
+	# Since this dependece on a number of components, esp. FFmpeg. Always rebuild.
+	cd $HOME
+	GitGet lsw $URL_LSW
+	cd "${PATH_SRC}/lsw/common"
+	cp $HOME/LSWAviUtl*.patch ./
+	for pfile in *.patch
+		do
+			patch -p0 -b -t -i $pfile
+		done
+	rm *.patch
+	cd "${PATH_SRC}/lsw/AviUtl"
+	./configure --prefix=$PATH_PREFIX  --extra-cflags="-static-libgcc -static-libstdc++" --extra-ldflags=" -static "|| exit $?
+	make || exit $?
+	printf "%d\n" $?
+	cp ${PATH_SRC}/lsw/AviUtl/*.au? ${PATH_PREFIX}/lib || exit $?
+	cd $HOME
+	
+}
+
+
+########## MAIN STARTS HERE ###############
+clear
+cd $HOME
+CreateFolder $PATH_SANDBOX
+CreateFolder $PATH_SRC
+CreateFolder $PATH_BLD
+CreateFolder $PATH_PREFIX
+
+PkgZLIB
+PkgLSMASH
+PkgFFMPEG
+PkgLSWAVIUTL
---
+++ lsw-bld3.ps1
@@ -0,0 +1,154 @@
+﻿param(
+    [string]$msysfolder = "C:\msys64"
+)
+
+# Hardcoded constant section
+$url_7z = "http://www.7-zip.org/a/7za920.zip"
+$url_msys2base = "https://sourceforge.net/projects/msys2/files/Base/x86_64/"
+$url_mingwbase = "https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/"
+$url_cmakebase = "https://cmake.org/download/"
+
+# Functions
+
+function Get-DiskSpace {
+# Get Free Disk space on installation drive as GB
+  param([string]$path)
+  $drive = Split-Path -Path $path -Qualifier
+  $drvfilter = "DeviceID='"+$drive+"'"
+  $driveinfo = GWMI Win32_LogicalDisk -Filter $drvfilter
+  $freespace = [Math]::Round($driveinfo.FreeSpace / 1GB)
+  return $freespace
+}
+
+function Install-MSYS2 {
+# Install MSYS2 to target folder
+# Download Mingw-w64 if missing
+  param([string]$path)
+  $targetdrive = Split-Path -Path $path -Qualifier
+  $temp = Join-Path -Path $targetdrive -ChildPath "msysinst"
+  if(!(Test-Path -Path $path)){
+    # Create a new installation if target folder does not exists
+    $free = Get-DiskSpace -path $path
+    if($free -lt 4){
+      Write-Output "Need at least 4GB on installation drive!"
+      Exit-PSSession 1
+    }
+    
+    cd $targetdrive
+    mkdir -Path $temp -Force
+    cd $temp
+
+    # Scrap Sourceforge download page for MSYS2
+    # Requires TLS1.2
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    $url_msys2 = (Invoke-WebRequest -Uri $url_msys2base).Links | Where({$_.href -like "*.tar.xz*"}) | Select-Object -First 1 -ExpandProperty href
+    #Write-Output $url_msys2
+
+    # Download MSYS2 and 7zip
+    Write-Output "Downloading 7zip and MSYS2"
+    Start-BitsTransfer -Source $url_7z, $url_msys2 -Destination "7z.zip", "msys2.tar.xz"
+
+    # Unpack 7zip
+    Write-Output "Unpacking 7zip CLI"
+    Expand-Archive -Path "./7z.zip" -DestinationPath "./" -Force
+
+    # Unpack MSYS2
+    Write-Output "Unpacking MSYS2"
+    .\7za.exe x msys2.tar.xz
+    # got msys2.tar
+    rm msys2.tar.xz
+    .\7za.exe x msys2.tar
+    # Clean-up
+    rm msys2.tar
+    rm 7z.zip
+    # Move to target folder
+    mv msys64 $path
+
+    # Initialize MSYS2
+    # Ref: https://github.com/TeaCI/msys2-docker/blob/master/msys2-init
+    Write-Output "Initializing MSYS2 installation..."
+    cd $path
+    .\msys2_shell.cmd -c "pacman -Syy --noconfirm --noprogressbar pacman"
+    .\msys2_shell.cmd -c "pacman -Suu --needed --noconfirm --noprogressbar"
+    .\msys2_shell.cmd -c "pacman -Suu --needed --noconfirm --noprogressbar && pacman -Scc --noconfirm"
+    .\msys2_shell.cmd -c "pacman -S --needed --noconfirm --noprogressbar base-devel &&  pacman -Scc --noconfirm"
+    .\msys2_shell.cmd -c "pacman -S --needed --noconfirm --noprogressbar VCS && pacman -Scc --noconfirm"
+    .\msys2_shell.cmd -c "pacman -S --needed --noconfirm --noprogressbar yasm nasm nano p7zip unzip atool && pacman -Scc --noconfirm"
+    .\msys2_shell.cmd -c "cp -f /usr/bin/false /usr/bin/tput"
+    .\autorebase.bat
+  
+  }
+  # End of MSYS2 base installation
+
+  # Install CMake from Official Site
+  # NEVER Install CMake package from pacman !!!
+    cd $temp
+    
+    $url_cmake = (Invoke-WebRequest -Uri $url_cmakebase).Links | Where({$_.href -like "*x64.zip"})| Where ({$_.href -notlike "*rc*"}) | Select-Object -First 1 -ExpandProperty href
+    $url_cmake = "https://cmake.org$($url_cmake)"
+    Write-Output $url_cmake
+    Start-BitsTransfer -Source $url_cmake -Destination "cmake.zip"
+    Expand-Archive -Path "./cmake.zip" -DestinationPath "./" -Force
+    $cmakeroot= ls -Name -Directory cmake*
+    $cmakebin = Join-Path -Path $cmakeroot -ChildPath "/bin"
+    $cmakeaclocal = Join-Path -Path $cmakeroot -ChildPath "/share/aclocal"
+    $cmakesharefolder = ls -Name -Directory "$($cmakeroot)/share/cmake*"
+    $cmakeshare = Join-Path -Path $cmakeroot -ChildPath "/share/$($cmakesharefolder)"
+    Copy-Item -Path "$($cmakebin)\*.exe" -Destination $(Join-Path -Path $path -ChildPath "/usr/bin" )
+    Copy-Item -Path "$($cmakeaclocal)\*.*" -Destination $(Join-Path -Path $path -ChildPath "/usr/share/aclocal")
+    Copy-Item -Path $cmakeshare -Recurse -Container -Destination $(Join-Path -Path $path -ChildPath "/usr/share" )
+
+  # Get Mingw-w64 SJLJ if missing
+  $targetmingw = Join-Path -Path $path -ChildPath "i686-posix-sjlj"
+  if(!(Test-Path -Path $targetmingw)){
+    if(!(Test-Path -Path $temp)){
+      mkdir -Path $temp -Force
+    }
+    cd $temp
+    # Get Mingw-w64 Link
+    $url_mingw = (Invoke-WebRequest -Uri $url_mingwbase).Links | Where({$_.href -like "*i686*posix-sjlj*"}) | Select-Object -First 1 -ExpandProperty href
+    # Download Mingw-w64
+    Write-Output "Downloading Mingw-w64 (32bit toolchain)"
+    Start-BitsTransfer -Source $url_mingw -Destination i686-posix-sjlj.7z
+    # DL 7zip if missing
+    if(!(Test-Path -Path "./7za.exe")){
+      Start-BitsTransfer -Source $url_7z -Destination "7z.zip"
+      Expand-Archive -Path "./7z.zip" -DestinationPath "./" -Force
+    }
+    # Extract Mingw-w64
+    ./7za.exe x i686-posix-sjlj.7z
+    # Rename and move Mingw-w64
+    $targetmingw = Join-Path -Path $path -ChildPath "i686-posix-sjlj"
+    mv mingw32 $targetmingw
+  }
+  cd $PSScriptRoot
+  # Delete temp folder
+  if(Test-Path -Path $temp){
+    Remove-Item -Path $temp -Recurse -Force
+  }
+  # Copy build script
+  Copy-Item -Path ".\*.sh" -Destination $(Join-Path -Path $path -ChildPath "/home/$($env:USERNAME)" )
+  # Copy patch
+  Copy-Item -Path ".\*.patch" -Destination $(Join-Path -Path $path -ChildPath "/home/$($env:USERNAME)" )
+}
+
+
+
+# MAIN
+Clear-Host
+Install-MSYS2 -path $msysfolder
+Write-Output "Finished MSYS2 Setup"
+Write-Output "Building..."
+cd $path
+$sw = [Diagnostics.Stopwatch]::StartNew()
+.\msys2_shell.cmd -c "source ~/buildme"
+$sw.Stop()
+$sw.Elapsed
+Write-Output "AviUtl Plugins are inside ~/Sandbox/Install/lib"
+Write-Output "L-SMASH binaries are inside ~/Sandbox/Install/bin"
+Write-Output "To build updates, run $($path)/msys2.exe "
+Write-Output "then invoke ./buildme "
+
+
+
